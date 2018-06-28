af34cb9898acafce11fe1545cbef5b3cc00dc710
thnikk <thnikk@github.com>
2018-06-28 03:46:20 -0700

    Updated to bootatrap 4

--- share/htag.php
+++
@@ -1,9 +0,0 @@
-<meta name="description" content="Keypad website">
-<meta name="author" content="thnikk">
-<link rel="icon" href="http://thnikk.moe/favicon.png?v=3">
-
-<!-- Bootstrap core CSS -->
-<link href="http://thnikk.moe/css/bootstrap.min.css" rel="stylesheet">
-<!-- Custom styles for this template -->
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="http://thnikk.moe/css/test.css" rel="stylesheet">
